7c92052c09da3ccd87f31184bfa503572ab3fa17
선호\tjsgh <tjsgh9414@naver.com>
2023-11-16 22:41:35 +0900

    db load

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  class Board extends CI_Controller {
     function __construct() {       
         parent::__construct();
-        $this -> load -> database('ssnong');
+        $this -> load -> database();
         $this -> load -> model('board_model');
         $this -> load -> helper(array('url', 'date'));
       }
