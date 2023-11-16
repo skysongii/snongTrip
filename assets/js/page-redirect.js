4b76efa61eadcdca8d5fa4f4fa954782a2fe638a
선호\tjsgh <tjsgh9414@naver.com>
2023-11-16 22:23:35 +0900

    js 자유게시판 리다이렉트

--- assets/js/page-redirect.js
+++ assets/js/page-redirect.js
@@ -1,9 +1,10 @@
 
 /**
  * 2023-11-01
- * @author: csh
+ * @author -> csh
  * Open API 페이지 리다이렉트
  */
+
 let goWhois = () => {
     setTimeout(() => {
         location.href = '/WhoisController';
@@ -13,7 +14,27 @@ let goWhois = () => {
     // console.log(1);
 }
 
-// 준비중
+/**
+ * 2023-11-16
+ * @author -> csh
+ * 자유게시판 리다이렉트
+ */
+
+let goBoard = () => {
+    setTimeout(() => {
+        location.href = '/board';
+    }, 2000);
+    var element = document.getElementById('change-typed')
+    element.innerHTML = '<h2 id="change-typed" class="blink">  자유게시판 페이지로 이동합니다</h2>';
+}
+
+
+
+/**
+ * 2023-11-01
+ * @author -> csh
+ */
+
 const readyToPage = () => {
     alert("곧 찾아뵙겠습니다..ㅠㅠ");
 }
