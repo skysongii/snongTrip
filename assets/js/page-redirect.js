0a85bf94a47bf8c25dd7511cfb8b694891deb462
선호\tjsgh <tjsgh9414@naver.com>
2023-11-20 22:40:34 +0900

    js 테스트

--- assets/js/page-redirect.js
+++ assets/js/page-redirect.js
@@ -23,7 +23,7 @@ let goWhois = () => {
 let goBoard = () => {
     setTimeout(() => {
         location.href = '/Board';
-        console.log(1);
+        console.log("준비중입니다");
     }, 2000);
     var element = document.getElementById('change-typed')
     element.innerHTML = '<h2 id="change-typed" class="blink">  자유게시판 페이지로 이동합니다</h2>';
