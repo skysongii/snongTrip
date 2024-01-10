1cb1943cf8ef333c9e2e142242e06c45d8c8bb78
선호\tjsgh <tjsgh9414@naver.com>
2024-01-10 13:13:00 +0900

    국내여행 Maps API - 모달입력

--- maps/assets/js/modal.js
+++ maps/assets/js/modal.js
@@ -1,13 +1,15 @@
-let modal_cancel = document.getElementById('modal-cancel');
-let modal_save = document.getElementById('modal-save');
-
+let modal_cancel    = document.getElementById('modal-cancel'); // 모달창 닫기
+let modal_save      = document.getElementById('modal-save');     // 모달창 저장
+let modal_stay      = document.getElementById('modal-stay');
+let modal_food      = document.getElementById('modal-food');
+let modal_place      = document.getElementById('modal-place');
 
 // 모달창 등록버튼 클릭
 const modal = document.querySelector('.modal');
 const btnOpenModal=document.querySelector('#content-insert');
 
 btnOpenModal.addEventListener("click", ()=>{
-    modal.style.display="flex";
+    modal.style.display = "flex";
 });
 
 // 모달창 닫기
@@ -17,5 +19,10 @@ modal_cancel.addEventListener('click', function () {
 
 // 모달창 저장버튼
 modal_save.addEventListener('click', function () {
-    
+    let stay_val    = modal_stay.value;
+    let food_val    = modal_food.value;
+    let food_place  = modal_place.value;
+    // console.log(stay_val);
+    // console.log(food_val);
+    // console.log(food_place);
 });
