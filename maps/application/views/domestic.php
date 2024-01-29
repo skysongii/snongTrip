4ee3f1afc20639532a8ddde8eb563945fbf5d601
선호\tjsgh <tjsgh9414@naver.com>
2024-01-29 09:05:22 +0900

    메인화면 수정

--- maps/application/views/domestic.php
+++ maps/application/views/domestic.php
@@ -56,9 +56,6 @@
         </div>
     </nav>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Open modal for @fat</button>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button>
-
 <!-- 모달 등록창 -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
