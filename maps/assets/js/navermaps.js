914e68c6e700d6183742bfa3f54991a9024598c5
skysongii <tjsgh9414@naver.com>
2024-02-04 23:01:49 +0900

    수정

--- maps/assets/js/navermaps.js
+++ maps/assets/js/navermaps.js
@@ -283,7 +283,7 @@ let locationChkFunc = () => {
 /**
  * @author	: csh
  * @date	: 2024-02-02
- * insertFnlChk - 등록 버튼 클릭 시 확인
+ * insertFnlChk - 등록 버튼 클릭 시 검증
  * {
  * 	@location 	: 주소 0,1
  *  @start_date : 시작날짜 0,1
